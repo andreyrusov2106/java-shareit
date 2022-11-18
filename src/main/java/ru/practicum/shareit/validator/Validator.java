374b9ca914fa4b02f1d7aa781a893e72bfa4c4d5
andrey.rusov <rusov2106@mail.ru>
2022-11-18 12:19:51 +0300

    add items and users

--- src/main/java/ru/practicum/shareit/validator/Validator.java
+++ src/main/java/ru/practicum/shareit/validator/Validator.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.validator;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+
 @Validated
 public interface Validator<T> {
     void check(@Valid T t);
