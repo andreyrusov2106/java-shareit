e54390f3f6a7ef266c85f927ed2ae9dbaddf8f2e
andrey.rusov <rusov2106@mail.ru>
2022-12-19 17:28:56 +0300

    add bookings and comments

---
+++ src/test/java/ru/practicum/shareit/integration/item/ItemServiceImplTest.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.integration.item;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@SpringBootTest(
+        properties = "db.name=test1",
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class ItemServiceImplTest {
+    private final ItemService itemService;
+    private final UserService userService;
+    @Test
+    @Order(1)
+    void testSaveItem() {
+        UserDto userDto = UserDto.builder().id(1L).email("user1@mail.ru").name("User1").build();
+        UserDto user = userService.createUser(userDto);
+        ItemDto itemDto = ItemDto.builder().description("Item1desc").name("Item1").available(true).build();
+        ItemDto item = itemService.createItem(itemDto, 1L);
+        assertThat(item.getId(), notNullValue());
+        assertThat(item.getName(), equalTo(itemDto.getName()));
+        assertThat(item.getDescription(), equalTo(itemDto.getDescription()));
+        assertThat(item.getAvailable(), equalTo(itemDto.getAvailable()));
+    }
+}
