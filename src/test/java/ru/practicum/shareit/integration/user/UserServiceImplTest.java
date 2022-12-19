631574ff8e56d259859015c289c1aaf87cfd1135
andrey.rusov <rusov2106@mail.ru>
2022-12-19 16:44:24 +0300

    add bookings and comments

--- src/test/java/ru/practicum/shareit/integration/user/UserServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/integration/user/UserServiceImplTest.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -20,6 +23,7 @@ public class UserServiceImplTest {
     private final UserService service;
 
     @Test
+    @Order(1)
     void testSaveUser() {
         UserDto userDto = UserDto.builder().id(1L).email("user1@mail.ru").name("User1").build();
         UserDto user = service.createUser(userDto);
@@ -29,11 +33,37 @@ public class UserServiceImplTest {
     }
 
     @Test
+    @Order(2)
     void testUpdateUser() {
         UserDto userDto2 = UserDto.builder().email("user1@mail.ru").name("User1New").build();
         UserDto user = service.updateUser(1L, userDto2);
         assertThat(user.getId(), notNullValue());
         assertThat(user.getEmail(), equalTo(userDto2.getEmail()));
         assertThat(user.getName(), equalTo(userDto2.getName()));
+    }
+
+    @Test
+    @Order(3)
+    void testGetAllUsers() {
+        List<UserDto> users = service.getAllUsers();
+        assertThat(users.size(), equalTo(1));
+
+    }
+
+    @Test
+    @Order(4)
+    void testGetUser() {
+        UserDto userDto2 = UserDto.builder().email("user1@mail.ru").name("User1New").build();
+        UserDto user = service.getUser(1L);
+        assertThat(user.getId(), notNullValue());
+        assertThat(user.getEmail(), equalTo(userDto2.getEmail()));
+        assertThat(user.getName(), equalTo(userDto2.getName()));
+    }
+    @Test
+    @Order(5)
+    void testRemoveUser() {
+        service.removeUser(1L);
+        List<UserDto> users = service.getAllUsers();
+        assertThat(users.size(), equalTo(0));
     }
 }
